d7edf5b380da41bf9d5093766a62a19f9b1d501e
Syed Adees <adees.gillani@gmail.com>
2014-12-03 22:01:25 +0500

    Create Factorial.cpp
    This program gives you the factorial of the number you entered using only loop statement in C++

---
+++ Factorial/Factorial.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+	int a = 1, b = 1, c;
+	cout << "Enter any number : \n";
+	cin >> c;
+
+	for (a = 1; a <= c; a++) 
+	// a strarts from 1 and is less then the number you entered. a++ shows the increment ( a=a+1 ).
+	b = b*a; 
+	// b is equal to 1 x the value of a ( a=a+1 || a=a+2 || a=a+3 ) 
+	  {
+		cout << "The factorial is : " << b << endl;
+	   }
+	_getch(); 
+	return 0;
+}
